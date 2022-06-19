d9f85225d48e3073ab64e697bdc2e5b3004ec978
JeffersonMartins <jeffersonmartins.jef@gmail.com>
2022-06-19 09:37:14 -0300

    Adicionada a lógica para saber as cartas clicadas são iguais ou não

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
 let cards = document.querySelectorAll('.memory-card');
 
+let temCartasViradas = false;
+let primeiraCarta, segundaCarta;
+
 function flipCard(){
-    this.classList.toggle('flip');
+    
+    this.classList.add('flip');
+
+    if (!temCartasViradas){
+        temCartasViradas = true;
+        primeiraCarta = this;
+        return;
+    }
+
+    segundaCarta = this;
+    temCartasViradas = false;
+
+    checkForMatch();
+}
+
+function  checkForMatch() {
+    if (primeiraCarta.dataset.framework === segundaCarta.dataset.framework){
+        desativarCartas();
+        return;
+    }
+
+    desvirarCartas();
+}
+
+function desativarCartas(){
+    primeiraCarta.removeEventListener('click', flipCard);
+    segundaCarta.removeEventListener('click', flipCard);
+}
+
+function desvirarCartas(){
+    setTimeout(() => {
+        primeiraCarta.classList.remove('flip');
+        segundaCarta.classList.remove('flip');
+    }, 1500)
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
